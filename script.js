9b52e82ea34101ad626630682ee26cbde32533ae
ziedll <112785620+ziedll@users.noreply.github.com>
2026-08-17 12:38:39 +0100

    Ajout de l'API sur la partie droite du site (himidité, pression, qualité de l'air, heure de lever et de coucher du soleil,etc...) et ajout de l'API de la partie de prévisions sur 5 jours qui annonce la date le jour et la température des 5 prochains jours, ajout de la localisation exacte en dessous de la température du pays que nous cherchons.

--- script.js
+++ script.js
@@ -17,11 +17,20 @@ async function fetchDatawithHeader(){
 let cityInput = document.getElementById("city");
 let searchBtn = document.getElementById("search");
 let API_key = "2548d3426398a0d799d768b43669be0a";
-let currentWeatherCard = document.querySelectorAll('.weather-left .card')[0];
+let currentWeatherCard = document.querySelectorAll(".weather-left .card")[0];
+let fiveDaysForecastCard = document.querySelector(".future-weather");
+let aqiCard = document.querySelectorAll(".highlights .card")[0];
+let aqiList = ["Good", "Fair", "Moderate", "Poor", "Very Poor"];
+let sunriseCard = document.querySelectorAll(".highlights .card")[1];
+let humidityVal = document.getElementById('humidityVal');
+let pressureVal = document.getElementById('pressureVal');
+let visibilityVal = document.getElementById('visibilityVal');
+let windSpeedVal = document.getElementById('windSpeedVal');
+let feelsLikeVal = document.getElementById('feelsLikeVal');
 function getWeatherDetails(name, lat, lon, country, state) {
     let FORECAST_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_key}`;
     let WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`;
-    
+    let AIR_POLLUTION_API_URL = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_key}`;
     let days = [
         "Sunday",
         "Monday",
@@ -45,7 +54,8 @@ function getWeatherDetails(name, lat, lon, country, state) {
         "Nov",
         "Dec",
     ];
-    fetch(WEATHER_API_URL).then((res) => res.json())
+    fetch(WEATHER_API_URL)
+        .then((res) => res.json())
         .then((data) => {
             let date = new Date();
             currentWeatherCard.innerHTML = `
@@ -59,25 +69,132 @@ function getWeatherDetails(name, lat, lon, country, state) {
             <hr>
             <div class="card-footer">
                 <p><i class = "fa-light fa-calendar"></i>${days[date.getDay()]}, ${date.getDate()}, ${months[date.getMonth()]}, ${date.getFullYear()}</p>
-                <p><i class="fa-light fa-location-dot"></i>___</p>
+                <p><i class="fa-solid fa-location-dot"></i>${country}</p>
+                <p><i class="fa-solid fa-location-dot"></i>${name}</p>
+                <p><i class="fa-solid fa-location-dot"></i>${state}</p>
             </div>`;
+            const timezoneOffsetInSeconds = data.timezone;
+
+            const sunriseLocal = moment
+                .utc((data.sys.sunrise + timezoneOffsetInSeconds) * 1000)
+                .format("HH:mm ");
+            const sunsetLocal = moment
+                .utc((data.sys.sunset + timezoneOffsetInSeconds) * 1000)
+                .format("HH:mm ");
+                let {visibility} = data;
+                let {humidity, pressure, feels_like} = data.main;
+                let {speed} = data.wind;
+            sunriseCard.innerHTML = `
+            <div class="card-head">
+                                <p>Sunrise & Sunset</p>
+                            </div>
+                            <div class="sunrise-sunset">
+                                <div class="item">
+                                    <div class="icon">
+                                        <i class="fa-solid fa-cloud-sun-rain fa-4x"></i>
+                                    </div>
+                                    <div> 
+                                    <p>Sunrise</p>
+                                <h2>${sunriseLocal}</h2></div>
+                                </div>
+                                <div class="item">
+                            <div class="icon">
+                                <i class="fa-solid fa-cloud-sun-rain fa-4x"></i>
+                            </div>
+                            <div>
+                                <p>
+                                    Sunset
+                                </p>
+                                <h2>${sunsetLocal}</h2>
+                            </div>
+                        </div>
+                            </div>`;
+                humidityVal.innerHTML = `${humidity}%`;
+                pressureVal.innerHTML = `${pressure}hPa`;
+                visibilityVal.innerHTML= `${visibility / 1000} km`;
+                windSpeedVal.innerHTML = `${speed}m/s`;
+                feelsLikeVal.innerHTML = `${(feels_like - 273.15).toFixed(2)} &deg;C`;
         })
         .catch(() => {
             alert("Failed to fetch");
         });
-        fetch(FORECAST_API_URL).then((res)=> res.json()).then((data)=> {
+    fetch(FORECAST_API_URL)
+        .then((res) => res.json())
+        .then((data) => {
             let uniqueForeCastDays = [];
-            let fiveDaysForecast = data.list.filter(forecast =>{
-                let forecastDate = new Date (forecast.dt_txt).getDate();
-                if(!uniqueForeCastDays.includes(forecastDate)){
+            let fiveDaysForecast = data.list.filter((forecast) => {
+                let forecastDate = new Date(forecast.dt_txt).getDate();
+                if (!uniqueForeCastDays.includes(forecastDate)) {
                     return uniqueForeCastDays.push(forecastDate);
                 }
             });
-            console.log(fiveDaysForecast);
-        }).catch(()=>{
-            alert('failed to fetch');
+            fiveDaysForecastCard.innerHTML = "";
+            for (i = 1; i < fiveDaysForecast.length; i++) {
+                let date = new Date(fiveDaysForecast[i].dt_txt);
+                fiveDaysForecastCard.innerHTML += `
+                <div class="weather-item">
+                        <div class="icon-wrapper">
+                            <img src="https://openweathermap.org/img/wn/${fiveDaysForecast[i].weather[0].icon}.png" alt="image">
+                            <span>${(fiveDaysForecast[i].main.temp - 273.15).toFixed(2)}&deg;C</span>
+                        </div>
+                        <p>${date.getDate()} ${months[date.getMonth()]}</p>
+                        <p>${days[date.getDay()]}</p>
+                    </div>`;
+            }
+        })
+        .catch(() => {
+            alert("failed to fetch");
+        });
+    fetch(AIR_POLLUTION_API_URL)
+        .then((res) => res.json())
+        .then((data) => {
+            let { co, no, no2, o3, so2, pm2_5, pm10, nh3 } = data.list[0].components;
+            aqiCard.innerHTML = `
+            <div class="card-head">
+                            <p>Air Quality Index</p>
+                            <p class="air-index aqi-${data.list[0].main.aqi}">${aqiList[data.list[0].main.aqi - 1]}</p>
+                        </div>    
+                        <div class="air-indices">
+                            <i class="fa-solid fa-wind fa-3x"></i>
+                            <div class="item">
+                                <p>PM2.5</p>
+                                <h2>${pm2_5}</h2>
+                            </div>
+                             <div class="item">
+                                <p>PM10</p>
+                                <h2>${pm10}</h2>
+                            </div>
+                             <div class="item">
+                                <p>SO2</p>
+                                <h2>${so2}</h2>
+                            </div>
+                             <div class="item">
+                                <p>CO</p>
+                                <h2>${co}</h2>
+                            </div>
+                             <div class="item">
+                                <p>NO</p>
+                                <h2>${no}</h2>
+                            </div>
+                             <div class="item">
+                                <p>NO2</p>
+                                <h2>${no2}</h2>
+                            </div>
+                              <div class="item">
+                                <p>NH3</p>
+                                <h2>${nh3}</h2>
+                            </div>
+                              <div class="item">
+                                <p>O3</p>
+                                <h2>${o3}</h2>
+                            </div>
+                        </div>`;
+        })
+        .catch(() => {
+            alert("Failed to fetch Air Quality Index");
         });
 }
+
 function getCityCoordinates() {
     let cityName = cityInput.value.trim();
     cityInput.value = "";
@@ -88,7 +205,7 @@ function getCityCoordinates() {
     fetch(GEOCODING_API_URL)
         .then((res) => res.json())
         .then((data) => {
-            let { name, lat, lon, country, state} = data[0] ;
+            let { name, lat, lon, country, state } = data[0];
             getWeatherDetails(name, lat, lon, country, state);
         })
         .catch(() => {
